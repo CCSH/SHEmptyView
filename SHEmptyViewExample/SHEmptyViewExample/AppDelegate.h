0c9f46e692b5e3009210c384f138b9607f383e2d
CCSH <624089195@qq.com>
2020-09-23 09:18:32 +0800

    no message

---
+++ SHEmptyViewExample/SHEmptyViewExample/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SHEmptyViewExample
+//
+//  Created by CSH on 2020/9/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ SHEmptyViewExample/SHEmptyViewExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SHEmptyViewExample
+//
+//  Created by CSH on 2020/9/23.
+//
+
+#import "ViewController.h"
+#import "SHEmptyView.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    SHEmptyView *view = [[SHEmptyView alloc]init];
+    view.frame = CGRectMake(0, 30, self.view.width, 200);
+    [self.view addSubview:view];
+    
+    [view reloadView];
+
+}
+
+
+@end
